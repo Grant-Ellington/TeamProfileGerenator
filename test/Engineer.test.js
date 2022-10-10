fd417680a17301b53bde82a757e923c5b3e97c89
Grant-Ellington <gellingtonem6@gmail.com>
2022-10-10 16:13:44 -0500

    added engineer test

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -2,9 +2,32 @@
 
 const Engineer = require('../lib/Engineer')
 
-test("Can set enginner name via constructor", () => {
+test("Can set engineer name via constructor", () => {
     let e =  new Engineer('Grant', '1234', 'test@test.com', 'name-name', 'Intern')
-    let results = 'Grant'
+    let results = 'Grant';
     expect(e.engineerName).toBe(results)
+});
+test("Can set engineer ID via constructor", ()=> {
+    let e = new Engineer ('Grant', '1234', 'test@test.com', 'name-name', 'Intern')
+    let results = '1234';
+    expect(e.engineerId).toBe(results)
+});
+
+test("Can set engineer Email via construuctor", ()=> {
+    let e = new Engineer ('Grant', '1234', 'test@test.com', 'name-name', 'Intern');
+    let results = 'test@test.com';
+    expect(e.engineerEmail).toBe(results)
+});
+
+test("Can set enginner GitHub via constructor", ()=> {
+    let e = new Engineer ('Grant', '1234', 'test@test.com', 'name-name', 'Intern')
+    let results = 'name-name'
+    expect(e.engineerGit).toBe(results)
+});
+
+test("Can set next choice via constructor", () => {
+    let e = new Engineer ('Grant', '1234', 'test@test.com', 'name-name', 'Intern');
+    let results = 'Intern'
+    expect(e.engineerChoice).toBe(results)
 });
 
