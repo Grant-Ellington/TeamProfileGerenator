9c9f61939242d3c76d6e483262ce289fc7a85fdc
Grant-Ellington <gellingtonem6@gmail.com>
2022-10-10 11:20:18 -0500

    changed syntax for engineer.test.js

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -11,15 +11,14 @@ test("should HTML block to cards array", () => {
         engineerChoices: 'Intern'
       }
     const test = createEngineerCard(array)
-    
-    expect(test).toBe(
-    `<div class="card" style="width: 18rem;">
+
+    const results = `<div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">'g'</h5>
       <h6 class="card-subtitle mb-2 text-muted">Engineer</h6>
       <p class="card-text">'g'</p>
-      <a href="mailto:'g'">'g'</a>
-      <a href="#" class="card-text">'g'</a>
-    </div>`)
+      <a href="mailto:'g'">'g'</a>`
+    
+    expect(test).toBe(results)
 })
 
